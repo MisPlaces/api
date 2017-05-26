b5bff8dfe9a5fd7379172dffc191889c70a015c5
Raul Neis <raulneis@gmail.com>
2017-05-26 20:56:14 -0300

    Laravel <3

---
+++ app/Http/Middleware/EncryptCookies.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace misplaces-api\Http\Middleware;
+
+use Illuminate\Cookie\Middleware\EncryptCookies as BaseEncrypter;
+
+class EncryptCookies extends BaseEncrypter
+{
+    /**
+     * The names of the cookies that should not be encrypted.
+     *
+     * @var array
+     */
+    protected $except = [
+        //
+    ];
+}
